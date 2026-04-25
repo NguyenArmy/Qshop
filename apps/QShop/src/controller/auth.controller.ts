b21d716af315a52cff68110d1c0abbc1dc47505f
NguyenArmy <anhquan109380@gmail.com>
2026-04-25 22:26:06 +0700

    add redis, nodemailer and fix some bugs

--- apps/QShop/src/controller/auth.controller.ts
+++ apps/QShop/src/controller/auth.controller.ts
@@ -1,9 +1,22 @@
 import { NextFunction, Request, Response } from "express";
+import { validationRegistrationData } from "../utils/auth.helper";
+import prisma from "../../../../packages/libs/prisma";
+import { ValidationError } from "../../../../packages/error-handler";
 
 
 
 
 //register a new user
 export const userRegister = async (req: Request, res: Response, next: NextFunction) => {
+  validationRegistrationData(req.body, "user");
+  const [name, email] = req.body;
 
+  const existingUser = await prisma.users.findUnique({
+    where: { email }
+  })
+  if (existingUser) {
+    return next(new ValidationError("Email already in use!"))
+  };
+  await checkOtpRestrictions(email, next);
 }
+
--- apps/QShop/src/utils/auth.helper.ts
+++ apps/QShop/src/utils/auth.helper.ts
@@ -1,5 +1,7 @@
 import crypto from "crypto";
 import { ValidationError } from "../../../../packages/error-handler";
+import redis from "../../../../packages/libs/redis";
+import { sendEmail } from "./sendMail";
 
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -12,5 +14,21 @@ export const validationRegistrationData = (data: any, userType: "user" | "seller
   if (!emailRegex.test(email)) {
     throw new ValidationError(`Invalid email format!`)
   }
+
+}
+export const checkOtpRestrictions = (email: string, next: NewableFunction) => {
+
+}
+
+export const sendOtp = async (name: string, email: string, template: string) => {
+  const otp = crypto.randomInt(1000, 9999).toString();
+
+  await sendEmail();
+
+  await redis.set(`otp:${email}`, otp, "EX", 300);
+  await redis.set(`otp_cooldown:${email}`, "true", "EX", 60);
+
+
+
 
 }
